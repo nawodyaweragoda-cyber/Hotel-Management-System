149105bbb0e84c89bc1649f501619227df4b557a
lakmindichamudi2-coder <lakmindichamudi2@gmail.com>
2026-08-15 12:00:16 +0530

    Add Billing, Review and Admin Dashboard APIs

---
+++ billing-review-service/src/main/java/com/hotel/billingreview/BillingReviewServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.billingreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingReviewServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingReviewServiceApplication.class, args);
+	}
+
+}
---
+++ billing-review-service/src/test/java/com/hotel/billingreview/BillingReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel.billingreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingReviewServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
